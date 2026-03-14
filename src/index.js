5dafce44ec43fc841e09509346fe57c8951e51a2
reivaj2dev <reivaj2dev@gmail.com>
2026-03-14 12:21:44 +0000

    Se inicializa el proyecto synapse-api

---
+++ src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mensaje = "TypeScript con Node 24 y pnpm listo!";
+console.log(mensaje);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,47 @@
+// src/index.ts
+import Fastify from 'fastify';
+import { loadSynapseConfig } from './loader.js';
+import { CONFIG } from './config/config.js'; //
+import { RouterBuilder } from './lib/core/RouterBuilder.js';
+import type { SynapseConfigFile } from './interfaces/synapse.js';
+
+const fastify = Fastify({ logger: true });
+
+async function bootstrap() {
+    try {
+        // Cargamos la configuración agrupada
+        const groupedConfig = loadSynapseConfig();
+        
+        // Forzamos el tipado de las entradas para evitar el error "unknown"
+        const entries = Object.entries(groupedConfig) as [string, SynapseConfigFile][];
+
+        for (const [groupKey, config] of entries) {
+            
+            // Resolvemos el prefijo
+            let apiPrefix = config.prefix || 'api/v{apiVersion:}';
+            apiPrefix = apiPrefix.replace('{apiVersion:}', config.apiVersion);
+
+            // Normalizamos la ruta
+            const finalPrefix = apiPrefix.startsWith('/') ? apiPrefix : `/${apiPrefix}`;
+
+            console.log(`[Synapse] Registrando versión ${config.apiVersion} en: http://${CONFIG.server.host}:${CONFIG.server.port}${finalPrefix}`);
+            console.log(config)
+            // Registramos el grupo en Fastify
+            await fastify.register(async (instance) => {
+                await RouterBuilder.build(instance, config);
+            }, { prefix: finalPrefix });
+        }
+
+        // Arrancamos con bind y port de la configuración
+        await fastify.listen({ 
+            port: CONFIG.server.port, 
+            host: CONFIG.server.bind 
+        });
+
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+}
+
+bootstrap();
